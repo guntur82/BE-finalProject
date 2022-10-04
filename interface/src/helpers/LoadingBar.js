6e13a80cfee1db929b727e51beb24e4c56955f12
muftialfarisy <alfabjp@gmail.com>
2022-10-04 14:16:27 +0700

    update_tombol_tabel

--- interface/src/helpers/LoadingBar.js
+++ interface/src/helpers/LoadingBar.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import Skeleton from "@mui/material/Skeleton";
 const LoadingBar = () => {
   return (
     <>
       <tr>
-        <td colSpan="10">Data tidak ada</td>
+        <td colSpan="10">
+          <Skeleton variant="circular"></Skeleton>
+        </td>
+        {/* <td colSpan="10">Data tidak ada</td> */}
       </tr>
     </>
   );
--- interface/src/pages/Home.js
+++ interface/src/pages/Home.js
@@ -1,27 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
-import LoadingBar from '../helpers/LoadingBar';
-import { useNavigate } from 'react-router-dom';
-import { AiOutlineQuestion } from 'react-icons/ai';
-import { FiCheck } from 'react-icons/fi';
-import Swal from 'sweetalert2';
-import { deleteCart, editCart, getCart } from '../axios/cartAxios';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import LoadingBar from "../helpers/LoadingBar";
+import { useNavigate } from "react-router-dom";
+import { AiOutlineQuestion } from "react-icons/ai";
+import { FiCheck } from "react-icons/fi";
+import Swal from "sweetalert2";
+import { deleteCart, editCart, getCart } from "../axios/cartAxios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
+import { styled } from "@mui/material/styles";
 
 const Home = () => {
   const navigate = useNavigate();
-
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [cart, setCart] = useState([]);
   useEffect(() => {
-    if (!localStorage.getItem('access_token')) {
-      navigate('/');
+    if (!localStorage.getItem("access_token")) {
+      navigate("/");
     }
     getCart((result) => setCart(result));
     // biar 1x load
@@ -31,15 +33,15 @@ const Home = () => {
     deleteCart(id);
   };
   const [status, setStatus] = useState({
-    status_barang: '',
-    status_pengiriman: '',
+    status_barang: "",
+    status_pengiriman: "",
   });
   const approveHandler = (id) => {
     status.status_barang = 1;
     status.status_pengiriman = 1;
     editCart(id, status, (result) => {
-      if (result.data.message === 'success') {
-        Swal.fire('Success', 'Pembaharuan berhasil', 'success');
+      if (result.data.message === "success") {
+        Swal.fire("Success", "Pembaharuan berhasil", "success");
       } else {
         console.log(result);
       }
@@ -47,24 +49,32 @@ const Home = () => {
   };
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-      backgroundColor: '#bfd1ec',
+      backgroundColor: "#bfd1ec",
       fontSize: 20,
       color: theme.palette.common.black,
-      fontStyle: 'bold',
+      fontStyle: "bold",
     },
     [`&.${tableCellClasses.body}`]: {
       fontSize: 14,
     },
   }));
   const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
     },
     // hide last border
-    '&:last-child td, &:last-child th': {
+    "&:last-child td, &:last-child th": {
       border: 0,
     },
   }));
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   return (
     <>
       <Navbar></Navbar>
@@ -91,69 +101,83 @@ const Home = () => {
                 </TableHead>
                 <TableBody>
                   {cart.length > 0 ? (
-                    cart.map((carts, key) => {
-                      const {
-                        id,
-                        jumlah,
-                        tanggal,
-                        status_barang,
-                        status_pengiriman,
-                      } = carts;
-                      if (status_barang === 1) {
-                        return (
-                          <StyledTableRow key={id}>
-                            <StyledTableCell align="center">
-                              {key + 1}
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                              {carts.user.name}
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                              {carts.item.name}
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                              {tanggal}
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                              {jumlah}
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                              {status_pengiriman === 0 ? (
-                                <AiOutlineQuestion />
-                              ) : (
-                                <FiCheck />
-                              )}
-                            </StyledTableCell>
-                            <StyledTableCell>
-                              {status_pengiriman === 0 ? (
-                                <>
-                                  <button
-                                    onClick={() => approveHandler(+id)}
-                                    className="btn btn-sm btn-primary"
-                                  >
-                                    Terima
-                                  </button>
-                                  <button
-                                    onClick={() => deleteHandler(+id)}
-                                    className="btn btn-sm btn-danger"
-                                  >
-                                    Tolak
-                                  </button>
-                                </>
-                              ) : (
-                                <>-</>
-                              )}
-                            </StyledTableCell>
-                          </StyledTableRow>
-                        );
-                      }
-                    })
+                    cart
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                      .map((carts, key) => {
+                        const {
+                          id,
+                          jumlah,
+                          tanggal,
+                          status_barang,
+                          status_pengiriman,
+                        } = carts;
+                        if (status_barang === 1) {
+                          return (
+                            <StyledTableRow key={id}>
+                              <StyledTableCell align="center">
+                                {key + 1}
+                              </StyledTableCell>
+                              <StyledTableCell align="center">
+                                {carts.user.name}
+                              </StyledTableCell>
+                              <StyledTableCell align="center">
+                                {carts.item.name}
+                              </StyledTableCell>
+                              <StyledTableCell align="center">
+                                {tanggal}
+                              </StyledTableCell>
+                              <StyledTableCell align="center">
+                                {jumlah}
+                              </StyledTableCell>
+                              <StyledTableCell align="center">
+                                {status_pengiriman === 0 ? (
+                                  <AiOutlineQuestion />
+                                ) : (
+                                  <FiCheck />
+                                )}
+                              </StyledTableCell>
+                              <StyledTableCell>
+                                {status_pengiriman === 0 ? (
+                                  <>
+                                    <button
+                                      onClick={() => approveHandler(+id)}
+                                      className="btn btn-sm btn-primary"
+                                    >
+                                      Terima
+                                    </button>
+                                    <button
+                                      onClick={() => deleteHandler(+id)}
+                                      className="btn btn-sm btn-danger"
+                                    >
+                                      Tolak
+                                    </button>
+                                  </>
+                                ) : (
+                                  <>-</>
+                                )}
+                              </StyledTableCell>
+                            </StyledTableRow>
+                          );
+                        }
+                      })
                   ) : (
                     <LoadingBar />
                   )}
                 </TableBody>
               </Table>
             </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={cart.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
           </div>
         </div>
       </div>
